0ede6d0b1e26f45e0e4367193a8fbe19357483bd
maru0pengin <maru0pengin@gmail.com>
2021-03-29 18:20:35 +0900

    画像のアップロードを実装

--- src/main.js
+++ src/main.js
@@ -3,20 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import firebase from 'firebase/app'
+import '@/plugins/element'
 
-Vue.config.productionTip = true
-
-
-if (firebase.apps.length === 0) {
-  firebase.initializeApp(JSON.parse(process.env.VUE_APP_FIREBASE_CONFIG))
-  console.log("test")
-}
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
-
-
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,81 @@
 <template>
-
-  <div class="home">
-      <div>
-    <el-upload >
-    <el-button><i />写真アップロード</el-button>
+  <div>
+    <el-upload action="" :http-request="upload" accept="image/*" :show-file-list="false">
+    <el-button><i class="el-icon-top" />写真アップロード</el-button>
     </el-upload>
-  </div>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p v-if="uploadedImages.src === null">選択されていません</p>
+    <img v-else :src="uploadedImages.src" class="upload-img" width="400" style="margin: 1rem">
+    <div style="text-align: center">
+    <el-button type="primary" @click="save">登録して次へ</el-button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import firebase from 'firebase/app'
+import 'firebase/storage'
+const ALREADY_UPLOADED = Symbol('ALREADY_UPLOADED')//ユニークで不変なデータ型
+const reader = new FileReader()
+const storageRef = firebase.storage().ref()
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: 'Upload',
+  data: () => ({
+    uploadedImages: {
+      src: null, // 画像イメージのソース
+      file: null, // 画像のFileオブジェクト。cloud storageに画像を保存済みの場合はFileオブジェクトではなくALREADY_UPLOADEDを入れる
+      date: null //IDとして使用する日付
+    }
+  }),
+  computed: {
+    /**
+     * Cloud Storageの保存先
+     */
+    storagePath() {
+      return `${this.date}`
+    }
+  },
+  props: {
+    msg: String
+  },
+  methods: {
+        upload({ file }) {
+      return new Promise((resolve, reject) => {
+        reader.readAsDataURL(file)
+        reader.onload = () => resolve(reader.result)
+        reader.onerror = reject
+      }).then(imageSrc => {
+        this.uploadedImages = { src: imageSrc, file }
+      }).catch(err => {
+        this.$rollbar.error(err)
+        this.$notify({ type: 'error', title: 'Error', message: '写真の読み込みに失敗しました。時間をおいて再度お試しください' })
+      })
+    },
+    save() {
+      this.date = new Date()
+      const { file } = this.uploadedImages
+      if (file === ALREADY_UPLOADED) {
+        // cloud storageにアップロード済みかつ、ユーザーが選択画像を変更していないとき
+        return this.goNext()
+      }
+      const ref = storageRef.child(this.storagePath)
+      console.log(ref)
+      ref.put(file).then(() => {
+        this.$notify({ type: 'success', title: 'Success', message: 'リファレンスチェックへのご協力ありがとうございました' })
+        this.goNext()
+      }).catch(err => {
+        this.$rollbar.error(err)
+        this.$notify({ type: 'error', title: 'Error', message: '名刺のアップロードに失敗しました。時間をおいて再度お試しください' })
+      })
+    },
+    goNext() {
+      //this.$router.push('home')
+      console.log("終了")
+    }
   }
 }
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
