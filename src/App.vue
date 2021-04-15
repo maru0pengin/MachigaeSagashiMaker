bca756ce4d13fa3aa48dacd7e8831aac0b207866
maru0pengin <maru0pengin@gmail.com>
2021-04-15 19:52:25 +0900

    ci_cd_test

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,33 @@
 <template>
   <div id="app">
-    <nav class="w-full flex bg-blue-300 lg:px-8 px-4 py-4 text-white items-center relative justify-between md:justify-start">
-      <div class="flex text-xl font-bold tracking-wider space-x-2 md:mx-0 mx-auto">
-        <router-link to="/"><p class="text-4xl no-underline text-grey-darkest hover:text-blue-dark font-bold">Machigae Sagashi</p></router-link>
+    ci_cd_test
+    <nav
+      class="w-full flex bg-blue-300 lg:px-8 px-4 py-4 text-white items-center relative justify-between md:justify-start"
+    >
+      <div
+        class="flex text-xl font-bold tracking-wider space-x-2 md:mx-0 mx-auto"
+      >
+        <router-link to="/"
+          ><p
+            class="text-4xl no-underline text-grey-darkest hover:text-blue-dark font-bold"
+          >
+            Machigae Sagashi
+          </p></router-link
+        >
       </div>
-      <div class="text-white flex md:space-x-2 p-0 md:px-0 md:space-y-0 space-y-1 md:ml-4 md:relative absolute top-full bg-blue-300 flex-col md:flex-row left-0 right-0">
-        <router-link to="/upload"><p class="text-xl font-bold px-4 py-2  hover:text-blue-100 rounded-lg block">問題投稿</p></router-link>
+      <div
+        class="text-white flex md:space-x-2 p-0 md:px-0 md:space-y-0 space-y-1 md:ml-4 md:relative absolute top-full bg-blue-300 flex-col md:flex-row left-0 right-0"
+      >
+        <router-link to="/upload"
+          ><p
+            class="text-xl font-bold px-4 py-2 hover:text-blue-100 rounded-lg block"
+          >
+            問題投稿
+          </p></router-link
+        >
       </div>
     </nav>
-    <router-view class="h-screen"/>
+    <router-view class="h-screen" />
   </div>
 </template>
 
@@ -19,18 +38,18 @@
   text-align: center;
   color: #2c3e50;
   font-family: arial black;
-  @apply bg-gray-50
+  @apply bg-gray-50;
 }
-.description{
-  @apply font-bold text-gray-900 text-xl text-blue-500
+.description {
+  @apply font-bold text-gray-900 text-xl text-blue-500;
 }
-.main_button{
-  @apply mt-4 inline-block px-6 py-2 font-medium leading-6 text-center text-white uppercase transition bg-blue-400 rounded shadow hover:shadow-lg hover:bg-indigo-500 focus:outline-none
+.main_button {
+  @apply mt-4 inline-block px-6 py-2 font-medium leading-6 text-center text-white uppercase transition bg-blue-400 rounded shadow hover:shadow-lg hover:bg-indigo-500 focus:outline-none;
 }
-.submit_button{
-  @apply mt-4 inline-block px-6 py-2 font-medium leading-6 text-center text-white uppercase transition bg-indigo-500 rounded shadow hover:shadow-lg hover:bg-indigo-800 focus:outline-none
+.submit_button {
+  @apply mt-4 inline-block px-6 py-2 font-medium leading-6 text-center text-white uppercase transition bg-indigo-500 rounded shadow hover:shadow-lg hover:bg-indigo-800 focus:outline-none;
 }
-.upload_button{
-  @apply inline-block px-4 py-2 font-medium leading-6 text-center text-blue-500 uppercase transition bg-transparent border-2 border-blue-500 rounded-md hover:bg-blue-100 focus:outline-none
+.upload_button {
+  @apply inline-block px-4 py-2 font-medium leading-6 text-center text-blue-500 uppercase transition bg-transparent border-2 border-blue-500 rounded-md hover:bg-blue-100 focus:outline-none;
 }
 </style>
