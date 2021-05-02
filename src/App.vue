802c10f9c1b20b5f8c131c943b644d543d1c557c
maru0pengin <maru0pengin@gmail.com>
2021-05-02 19:11:58 +0900

    fix:pixijsを用いたキャンバスが最小になるように修正

--- src/App.vue
+++ src/App.vue
@@ -50,9 +50,7 @@ Vue.use(VueClipboard);
 export default {
   metaInfo: {
     title: "Machigae Sagashi",
-    meta: [
-      { name: 'viewport', content: "width=420px,user-scalable=no"}
-    ]
+    meta: [{ name: "viewport", content: "width=420px,user-scalable=no" }],
   },
   methods: {
     gotoContact() {
@@ -76,6 +74,9 @@ export default {
 }
 .submit_button {
   @apply mt-4 inline-block px-6 py-2 font-medium leading-6 text-center text-white uppercase transition bg-indigo-500 rounded shadow hover:shadow-lg hover:bg-indigo-800 focus:outline-none;
+}
+.min_button {
+  @apply mt-2 inline-block px-1 py-1 w-16 text-sm font-medium leading-6 text-center text-white uppercase transition bg-blue-400 rounded shadow hover:shadow-lg focus:bg-indigo-500 focus:outline-none;
 }
 .upload_button {
   @apply inline-block px-4 py-2 font-medium leading-6 text-center text-blue-500 uppercase transition bg-transparent border-2 border-blue-500 rounded-md hover:bg-blue-100 focus:outline-none;
