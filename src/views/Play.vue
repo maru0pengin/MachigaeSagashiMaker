4c34957e35e2f5b90dcbc85242e18a802a9ac3a2
maru0pengin <maru0pengin@gmail.com>
2021-05-05 14:45:48 +0900

    updage:tweet機能をlocationを変更することで実装

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -33,29 +33,22 @@
         <button class="min_button mx-2" @click="createGameScene">
           再プレイ
         </button>
-        <div class="w-20 mx-2">
-          <a
-            href="https://twitter.com/share"
-            v-bind:data-url="location"
-            class="twitter-share-button"
-            data-hashtags="MachigaeSagashi,間違え探し"
-            data-size="large"
-            >Tweet</a
-          >
-        </div>
+        <button class="min_button mx-2" @click="tweet">
+          ツイート
+        </button>
       </div>
     </Modal>
   </div>
 </template>
 
 <script>
-import firebase from "firebase";
-const WIDTH = 400;
-const HEIGHT = 225;
+import firebase from "firebase"
+const WIDTH = 400
+const HEIGHT = 225
 
-import * as PIXI from "pixi.js"; // node_modulesから PIXI.jsをインポート
-import Loading from "@/components/Loading";
-import Modal from "@/components/Modal";
+import * as PIXI from "pixi.js" // node_modulesから PIXI.jsをインポート
+import Loading from "@/components/Loading"
+import Modal from "@/components/Modal"
 
 export default {
   data: function() {
@@ -75,33 +68,36 @@ export default {
       displayTimer: "",
       score: null,
       isCrear: false,
-    };
+    }
   },
   components: {
     Loading,
     Modal,
   },
   created: function() {
-    this.db = firebase.firestore(); // dbインスタンスを初期化
+    this.db = firebase.firestore() // dbインスタンスを初期化
   },
   computed: {
     id: function() {
-      return this.$route.params.id;
+      return this.$route.params.id
     },
     location: function() {
-      return location.href;
+      return location.href
+    },
+    tweetURL: function() {
+      const url = encodeURI(`${location.href}`)
+      return `http://twitter.com/intent/tweet?text=${this.displayTimer}秒で間違えを\n見つけられました！%20%23MachigaeSagashi&url=${url}`
     },
   },
   mounted: async function() {
-    //twttr.widgets.load()
     //間違え位置の取得
-    let docRef = await this.db.collection("quizzes").doc(this.id);
+    let docRef = await this.db.collection("quizzes").doc(this.id)
     docRef
       .get()
       .then(async (doc) => {
         if (doc.exists) {
-          this.title = doc.data().title;
-          this.name = doc.data().name;
+          this.title = doc.data().title
+          this.name = doc.data().name
           //間違えの位置を取得し、ステータス情報等を追加してdifferencesへ保存
           for (let i = 0; i < doc.data().differences.length; i++) {
             this.differences[i] = {
@@ -109,192 +105,188 @@ export default {
               status: 0,
               obj: null,
               circleObj: null,
-            };
+            }
           }
           //画像の取得
-          let ref;
+          let ref
           ref = await firebase
             .storage()
             .ref()
-            .child(`${this.id}/correct.png`);
+            .child(`${this.id}/correct.png`)
           await ref.getDownloadURL().then((url) => {
-            this.correctImgPath = url;
-          });
+            this.correctImgPath = url
+          })
           ref = await firebase
             .storage()
             .ref()
-            .child(`${this.id}/incorrect.png`);
+            .child(`${this.id}/incorrect.png`)
           await ref.getDownloadURL().then((url) => {
-            this.incorrectImgPath = url;
-          });
+            this.incorrectImgPath = url
+          })
 
-          this.app = new PIXI.Application({ width: WIDTH, height: HEIGHT });
-          let el = document.getElementById("canvas");
-          el.appendChild(this.app.view);
+          this.app = new PIXI.Application({ width: WIDTH, height: HEIGHT })
+          let el = document.getElementById("canvas")
+          el.appendChild(this.app.view)
 
           // ゲームcanvasのcssを定義する
           // ここで定義した画面サイズ(width,height)は実際に画面に表示するサイズ
-          this.app.renderer.view.style.position = "relative";
-          this.app.renderer.view.style.width = "400px";
-          this.app.renderer.view.style.height = "225px";
-          this.app.renderer.view.style.display = "block";
+          this.app.renderer.view.style.position = "relative"
+          this.app.renderer.view.style.width = "400px"
+          this.app.renderer.view.style.height = "225px"
+          this.app.renderer.view.style.display = "block"
 
           // canvasの背景色
-          this.app.renderer.backgroundColor = 0xffffff;
+          this.app.renderer.backgroundColor = 0xffffff
 
-          PIXI.Loader.shared.reset().add(this.incorrectImgPath);
+          PIXI.Loader.shared.reset().add(this.incorrectImgPath)
 
           // プリロード処理が終わったら呼び出されるイベント
           PIXI.Loader.shared.load((loader, resources) => {
             //resources["../assets/crear.mp3"].sound.play() // クリックで音が鳴る
-            this.resources = resources;
-            this.createGameScene();
-          });
+            this.resources = resources
+            this.createGameScene()
+          })
         } else {
           // doc.data() が未定義の場合
-          console.log("No such document!");
-          this.$router.push({ name: "Home", query: this.$route.query });
+          console.log("No such document!")
+          this.$router.push({ name: "Home", query: this.$route.query })
         }
       })
       .catch((error) => {
-        console.log("Error getting document:", error);
-        this.$router.push({ name: "Home", query: this.$route.query });
+        console.log("Error getting document:", error)
+        this.$router.push({ name: "Home", query: this.$route.query })
       })
       .finally(() => {
-        this.loading = false;
-      });
+        this.loading = false
+      })
   },
   methods: {
     createGameScene() {
-      this.timmer = 0;
-      this.isCrear = false;
+      this.timmer = 0
+      this.isCrear = false
 
       this.differences.forEach((difference) => {
-        difference.status = 0;
-      });
-      this.removeAllScene();
-      this.removeAllGameLoops();
+        difference.status = 0
+      })
+      this.removeAllScene()
+      this.removeAllGameLoops()
 
       // ゲーム用のシーンを生成
-      const gameScene = new PIXI.Container();
+      const gameScene = new PIXI.Container()
       // ゲームシーンを画面に追加
-      this.app.stage.addChild(gameScene);
+      this.app.stage.addChild(gameScene)
 
       const image = new PIXI.Sprite(
         this.resources[this.incorrectImgPath].texture
-      );
+      )
 
-      image.x = 0;
-      image.y = 0;
-      gameScene.addChild(image); // 間違え画像をシーンに追加
+      image.x = 0
+      image.y = 0
+      gameScene.addChild(image) // 間違え画像をシーンに追加
 
       //ヒットエリアの描画
-      const length = 30; //ヒットエリアの幅
-      const radius = 20; //正解時に出す縁の半径
+      const length = 30 //ヒットエリアの幅
+      const radius = 20 //正解時に出す縁の半径
 
       this.differences.forEach((difference) => {
-        difference.obj = new PIXI.Graphics();
+        difference.obj = new PIXI.Graphics()
         let rect = new PIXI.Rectangle(
           difference.x - length / 2,
           difference.y - length / 2,
           length,
           length
-        );
-        difference.obj.beginFill(0xfff000, 0.0); //ヒットエリアは透明
-        difference.obj.drawShape(rect);
-        difference.obj.endFill();
+        )
+        difference.obj.beginFill(0xfff000, 0.0) //ヒットエリアは透明
+        difference.obj.drawShape(rect)
+        difference.obj.endFill()
 
-        difference.obj.interactive = true; // クリック可能にする
-        difference.obj.hitArea = rect;
+        difference.obj.interactive = true // クリック可能にする
+        difference.obj.hitArea = rect
 
         difference.obj.on("pointerdown", function() {
           // クリック時に発動する関数
           if (difference.status === 0) {
             //正解を示す円を表示させる
-            difference.CircleObj = new PIXI.Graphics();
-            difference.CircleObj.lineStyle(5, 0xec6d71, 1);
+            difference.CircleObj = new PIXI.Graphics()
+            difference.CircleObj.lineStyle(5, 0xec6d71, 1)
             difference.CircleObj.drawCircle(
               difference.x,
               difference.y - radius / 2 + 10,
               radius,
               radius
-            );
-            gameScene.addChild(difference.CircleObj);
+            )
+            gameScene.addChild(difference.CircleObj)
             //正解数を増やす
             //this.upScore()
             //正解済みの間違えに設定
-            difference.status = 1;
+            difference.status = 1
           }
-        });
-        gameScene.addChild(difference.obj); //間違い範囲の図形をシーンに追加
-      });
-      this.addGameLoop(this.gameLoop);
+        })
+        gameScene.addChild(difference.obj) //間違い範囲の図形をシーンに追加
+      })
+      this.addGameLoop(this.gameLoop)
     },
     /**
      * 毎フレーム処理を追加する関数
      */
     addGameLoop(gameLoopFunction) {
-      this.app.ticker.add(gameLoopFunction); // 毎フレーム処理として指定した関数を追加
-      this.gameLoops.push(gameLoopFunction); // 追加した関数は配列に保存する（後で登録を解除する時に使う）
+      this.app.ticker.add(gameLoopFunction) // 毎フレーム処理として指定した関数を追加
+      this.gameLoops.push(gameLoopFunction) // 追加した関数は配列に保存する（後で登録を解除する時に使う）
     },
     removeAllScene() {
-      this.app.stage.removeChildren();
+      this.app.stage.removeChildren()
     },
     removeAllGameLoops() {
       // gameLoopsに追加した関数を全部tickerから解除する
       for (const gameLoop of this.gameLoops) {
-        this.app.ticker.remove(gameLoop);
+        this.app.ticker.remove(gameLoop)
       }
-      this.gameLoops = []; // gameLoopsを空にする
+      this.gameLoops = [] // gameLoopsを空にする
     },
     gameLoop() {
       // 毎フレームごとに処理するゲームループ
       // スコアテキストを毎フレームアップデートする
       this.score = this.differences.filter(function(difference) {
-        return difference.status === 1;
-      }).length;
+        return difference.status === 1
+      }).length
 
-      this.timer += 1 / 60;
-      this.displayTimer = this.timer.toFixed(2);
+      this.timer += 1 / 60
+      this.displayTimer = this.timer.toFixed(2)
 
       if (this.score === this.differences.length) {
-        this.createEndScene(); // 結果画面を表示する
+        this.createEndScene() // 結果画面を表示する
       }
     },
     createEndScene() {
       // 毎フレームイベントを削除
-      this.removeAllGameLoops();
+      this.removeAllGameLoops()
 
-      this.timer = 0;
-      this.isCrear = true;
-
+      this.timer = 0
+      this.isCrear = true
       !(function(d, s, id) {
         var js,
           fjs = d.getElementsByTagName(s)[0],
-          p = /^http:/.test(d.location) ? "http" : "https";
+          p = /^http:/.test(d.location) ? "http" : "https"
         if (!d.getElementById(id)) {
-          js = d.createElement(s);
-          js.id = id;
-          js.src = p + "://platform.twitter.com/widgets.js";
-          fjs.parentNode.insertBefore(js, fjs);
+          js = d.createElement(s)
+          js.id = id
+          js.src = p + "://platform.twitter.com/widgets.js"
+          fjs.parentNode.insertBefore(js, fjs)
         }
-      })(document, "script", "twitter-wjs");
+      })(document, "script", "twitter-wjs")
     },
     tweet() {
-      const url = encodeURI(`${location.href}`);
-      window.open(
-        `http://twitter.com/intent/tweet?text=${this.displayTimer}秒で間違えを\n見つけられました！&url=${url}`
-      );
+      location.href = this.tweetURL
     },
     gotoHome() {
-      this.$router.push({ name: "Home", query: this.$route.query });
+      this.$router.push({ name: "Home", query: this.$route.query })
     },
   },
   beforeDestroy() {
     //キャッシュからすべてのテクスチャを削除
-    PIXI.utils.clearTextureCache();
+    PIXI.utils.clearTextureCache()
   },
-};
+}
 </script>
 
 <style>
