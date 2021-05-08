d27d12f2f0ab998663b531892f46c9708eefe672
maru0pengin <maru0pengin@gmail.com>
2021-05-08 20:37:56 +0900

    update:functionからusersへの追加に成功

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const functions = require("firebase-functions");
 const admin = require('firebase-admin')
-admin.initializeApp(functions.config().firebase)
+admin.initializeApp()
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -14,8 +14,14 @@ admin.initializeApp(functions.config().firebase)
 var firestore = admin.firestore()
 
 exports.addUser = functions.auth.user().onCreate((user) => {
-  firestore().collection("users")
+  firestore.collection("users")
   .add({
     uid: user.uid,
+  }).then((docRef) => {
+    console.log("Document written with ID: ", docRef.id);
   })
+  .catch((error) => {
+    console.error("Error adding document: ", error);
+  });
+
 });
