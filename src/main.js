ec078f9f8246b975f2f7eb18663507d6bcca9f84
maru0pengin <maru0pengin@gmail.com>
2021-06-06 21:17:15 +0900

    update:NODE_ENVの出力確認

--- src/main.js
+++ src/main.js
@@ -11,6 +11,28 @@ import Firebase from "./firebase"
 Firebase.init()
 firebase.analytics()
 
+console.log(process.env.NODE_ENV)
+// import Rollbar from 'rollbar' // クライアント側で起きたエラーを収集してSlackに通知を飛ばす
+
+// if (process.env.NODE_ENV === 'production') {
+//   Vue.prototype.$rollbar = new Rollbar({
+//     accessToken: process.env.VUE_APP_ROLLBAR_ACCESS_TOKEN,
+//     captureUncaught: true,
+//     captureUnhandledRejections: true,
+//     payload: {
+//       environment: process.env.VUE_APP_ROLLBAR_ENV
+//     }
+//   })
+
+//   Vue.config.errorHandler = (err, vm, info) => {
+//     vm.$rollbar.error(err)
+//     throw err // rethrow
+//   }
+// } else {
+//   Vue.prototype.$rollbar = console
+// }
+
+
 new Vue({
   router,
   store,
