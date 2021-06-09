72285082f69bb4d3760b31c5beb3818aff267348
maru0pengin <maru0pengin@gmail.com>
2021-06-09 09:25:21 +0900

    update:ルーティングエラーへの対応

--- src/router/index.js
+++ src/router/index.js
@@ -87,7 +87,13 @@ const routes = [
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes:routes,
 })
+
+//自分自身を呼び出した際にエラーをキャッチ
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
 
 export default router
